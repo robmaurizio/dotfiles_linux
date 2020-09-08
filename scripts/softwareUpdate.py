5699158b44de94552b5114e37adc034abad97155
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2020-09-08 17:16:13 -0500

    removed reference to macOS after update
    Removed a line declaring the system ready to use that referenced macOS

--- scripts/softwareUpdate.py
+++ scripts/softwareUpdate.py
@@ -12,8 +12,6 @@ print('\nUpdates are done. Now for some cleanup...')
 # Cleanup after running updates
 os.system('sudo dnf autoremove')
 
-#print('Cleanup is done. Checking for macOS updates...\n')
-
 print('\nFinished! Your system is ready.\n')
 
 os.system('exit')
