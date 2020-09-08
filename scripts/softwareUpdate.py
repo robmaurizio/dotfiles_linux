9ea92be6526e9188b87ba042539e183bcbf6f71f
Rob Maurizio <45679381+robmaurizio@users.noreply.github.com>
2020-09-08 17:15:27 -0500

    swapped apt for dnf
    I moved to Fedora so dnf needs to be used instead of Debian's apt

--- scripts/softwareUpdate.py
+++ scripts/softwareUpdate.py
@@ -5,12 +5,12 @@ import os
 print('\nChecking for software updates...')
 
 # Updates the system
-os.system('sudo apt-get update && sudo apt-get upgrade -y')
+os.system('sudo dnf upgrade -y')
 
 print('\nUpdates are done. Now for some cleanup...')
 
 # Cleanup after running updates
-os.system('sudo apt autoremove')
+os.system('sudo dnf autoremove')
 
 #print('Cleanup is done. Checking for macOS updates...\n')
 
