20df37941432d942fe8b937df571021a4faa55a5
robmaurizio <45679381+robmaurizio@users.noreply.github.com>
2022-02-17 11:30:59 -0600

    more fixes

--- aliases.zsh
+++ aliases.zsh
@@ -8,6 +8,7 @@ alias q='exit'
 alias ls='ls --color=auto' # List files in the current directory
 alias la='ls -A' # List (all) files in the current directory
 alias ll='ls -alF' # List files as a list in the current directory
+alias ld='ls -d' # List directories only
 
 ###################
 #   Directories   #
@@ -24,14 +25,16 @@ alias whome='cd /mnt/c/Users/$USER'
 ###################
 #     System      #
 ###################
-alias ps='ps -acmx'
+alias ps='ps -e'
 alias root='sudo -i' # Allow root access
 alias sudo='sudo ' # Make aliases sudo-able
 alias zshrc='sudo edit /home/$USER/.zshrc'
+alias df='df -h'
 
 ###################
 #     Updates     #
 ###################
+alias apt='sudo apt'
 alias update='sudo apt update && sudo apt upgrade -y' # Look for application and Ubuntu updates
 alias reload='source /home/$USER/.zshrc'
 
