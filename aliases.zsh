bb690138182c6f18ffc46d5199fb6ce0bd2e912c
robmaurizio <45679381+robmaurizio@users.noreply.github.com>
2022-08-26 10:17:59 -0500

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -43,7 +43,7 @@ alias reload='source /home/$USER/.zshrc'
 ###################
 #  Applications   #
 ###################
-alias edit="nano"
+alias edit="gedit"
 alias gcom="git add . && git commit -m"
 alias gpull='git pull'
 alias top='htop'
